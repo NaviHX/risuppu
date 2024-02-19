c4df4402965496fdae86f03450fcd370a06d2002
NaviHX <navihx@foxmail.com>
2024-02-19 10:55:48 +0800

    fix: and_then borrow mut again

--- risuppu-std/src/base/and_then.rs
+++ risuppu-std/src/base/and_then.rs
@@ -3,11 +3,18 @@ use risuppu::{
     sexp::{Ptr, Sexp},
 };
 
-pub fn and_then(args: Ptr<Sexp>, env: &mut Env) -> Ptr<Sexp> {
+pub fn eval_cond(args: Ptr<Sexp>, env: &mut Env) -> Ptr<Sexp> {
+    let cond = args.car();
+    let cont = args.cdr();
+
+    let eval_cond = env.evaluate(cond);
+    Sexp::cons(eval_cond, cont)
+}
+
+pub fn and_then(args: Ptr<Sexp>, _env: &mut Env) -> Ptr<Sexp> {
     let e = args.car();
     let c = args.cdr().car();
 
-    let e = env.evaluate(e);
     if !e.is_nil() {
         Sexp::from_vec([c, e])
     } else {
@@ -17,7 +24,9 @@ pub fn and_then(args: Ptr<Sexp>, env: &mut Env) -> Ptr<Sexp> {
 
 #[cfg(test)]
 mod test {
-    use risuppu::{semantic::Env, sexp::Sexp};
+    use risuppu::{semantic::Env, sexp::{Sexp, parse::parse_sexp}};
+
+    use crate::base::load_base;
 
     #[test]
     fn something_and_then() {
@@ -47,5 +56,14 @@ mod test {
         let expanded = super::and_then(Sexp::from_vec([expr, cont]), &mut env);
         let evaluated = env.evaluate(expanded);
         assert_eq!(evaluated, Sexp::nil());
+    }
+
+    #[test]
+    fn embedded_and_then() {
+        let mut env = Env::new();
+        load_base(&mut env);
+        let expr = parse_sexp("(and-then (and-then (eq 1 1) (lambda (_) 1)) (lambda (_) #t))").unwrap().1;
+        let eval = env.evaluate(expr);
+        assert_eq!(eval, Sexp::bool(true));
     }
 }
--- risuppu-std/src/base/mod.rs
+++ risuppu-std/src/base/mod.rs
@@ -1,7 +1,9 @@
+use and_then::eval_cond;
+
 super::std_library!(
     base,
     (seq, "seq"),
     (r#do, "do"),
-    (and_then, "and-then"),
+    (and_then, "and-then", eval_cond),
     (r#let, "let")
 );
